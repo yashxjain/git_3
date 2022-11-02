d6812c54d6b9c2fafab8f1502e6f8a3068224657
unknown <jyash5332@gmail.com>
2022-11-02 17:16:06 +0530

    right

--- prime.js
+++ prime.js
@@ -1,2 +1,13 @@
-let num=0;
-for(i=0;i<=num;i++){
+let number=0;
+let a=13;
+for(let i=1;i<=a;i++){
+    if(a%i==2){
+        number++
+    }
+}
+if(number==2){
+    console.log("True")
+}else{
+    console.log("False")
+}
+}
